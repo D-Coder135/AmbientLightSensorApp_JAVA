51fdbd5685a69ba270c89e3ca18abd6b73c4a8fa
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-09 10:14:56 +0530

    implements the methods from the interface SensorEventListener

--- app/src/main/java/com/example/ambientlightsensorapp_java/MainActivity.java
+++ app/src/main/java/com/example/ambientlightsensorapp_java/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
@@ -26,5 +27,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
